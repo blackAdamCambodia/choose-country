5a2fb8e438cb5b630dac719aaf7371caff161257
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-07-19 02:42:16 +0700

    use startWith method for filter letter

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import countries from "./data/country-by-capital-city.json"
+const myStyle = {
+  listStyle: "none",
+  border: "1px solid",
+  padding: "10px",
+  backgroundColor: "#fcba03",
+  color: "black",
+  fontWeight: "bold",
+  fontSize: "20px",
+  textAlign: "left",
+}
+function myFilterCountry() {
+  return countries.filter(x => {
+    return  x.country.startsWith("B")
+  })
+}
+const colorName = "color:Green"
+  
+ 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+      <div>
+        <h1>Countries Starts With Letter "B"</h1>
+        <p>This is a list of all the countries Name Start With "B".</p>
+        <ul>
+          <li :style="myStyle" v-for="(country , index) in myFilterCountry()" :key="index"><span :style="colorName">{{index+1}}</span> .{{ country.country }}</li>
+        </ul>
+      </div>
+      
+    
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
